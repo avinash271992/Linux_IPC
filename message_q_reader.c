6377b4ad8fc637ac0a72aa578a9adebfac1a7528
avinash271992 <avinash.lala@gmail.com>
2017-08-05 11:34:57 +0530

    Message queue reader code

--- message_q_reader.c
+++ message_q_reader.c
@@ -1,1 +1,24 @@
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/ipc.h> 
+#include<sys/msg.h>
+#include<string.h>
+struct msgbuff
+{
+int mtype;
+char mtext[20];
+};
 
+int main(int argc,char **argv)
+{
+int id;
+id=msgget(1,IPC_CREAT|0666);
+struct msgbuff v;
+bzero(&v.mtext,20);
+v.mtype=atoi(argv[1]);
+if(msgrcv(id,&v,20,v.mtype,0)==-1)
+perror("msgrcv");
+else
+printf("Received message : %s",v.mtext);
+}
